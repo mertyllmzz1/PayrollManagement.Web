1aa3515a839287c01853cd930689668240bc5798
Your Name <you@example.com>
2025-12-05 09:45:32 +0300

    Create react app.Cleaning generated objects

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Payroll Management Web</h1>
+      <p>React frontend başarılı şekilde temizlendi.</p>
+    </div>
+  );
+}
+
+export default App;
